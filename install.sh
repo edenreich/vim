7e539a14d6ed5edf54f764a507017df18a3ebbd3
Eden Reich <eden.reich@gmail.com>
2020-09-06 22:00:12 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -1,35 +1,54 @@
 #!/bin/sh
 
-function install_for_linux
+install_for_linux()
 {
-	sudo apt-get -y update
-	
-	# todo install nodejs and npm for coc
+	apt-get update	
 
-	if [ ! vim ]; then
-		echo "vim is not installed. Installing vim..."
-		sudo apt-get install -y vim
+	if [ ! `which npm` ] || [ ! `which node` ]; then
+		echo '>> npm or node is not installed. installing nodejs...'
+		apt-get install -y nodejs
 	fi
 
-	if [ ! -d "$HOME/.vim/autoload/plug.vim..." ]; then
-		echo "Plug is not installed. Installing Plug"
+	if [ ! `which git` ]; then
+		echo '>> git is not installed. Installing git...'
+		apt-get install -y git
+	fi
+
+	if [ ! `which curl` ]; then
+		echo '>> curl is not installed. Installing curl...'
+		apt-get install -y curl
+	fi
+
+	if [ ! `which vim` ]; then
+		echo '>> vim is not installed. Installing vim...'
+		apt-get install -y vim
+	fi
+
+	if [ ! -d "$HOME/.vim/autoload/plug.vim" ]; then
+		echo '>> Plug is not installed. Installing Plug...'
 		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 	fi
 
-	vim +PlugInstall
+	echo '>> Downloading vim configurations...'
+	curl -sSL https://raw.githubusercontent.com/edenreich/vim-on-speeds/master/.vimrc -o $HOME/.vimrc
 
+	vim +'PlugInstall --sync'
 }
 
-function install_for_macos
+install_for_macos()
 {
 	if [ ! vim ]; then
-		echo "vim is not installed. Installing vim"
+		echo 'vim is not installed. Installing vim..'
 		brew install vim
 	fi
 }
 
-if [ "$OSTYPE" == 'linux-gnu' ]; then
+
+OS=`uname`
+if [ "$OS" = 'Linux' ]; then
+	echo '>> Installing on linux...' 
 	install_for_linux
-else
+elif [ "$OS" = 'Darwin' ]; then
+	echo '>> Installing on macOS...' 
 	install_for_macos
 fi	
