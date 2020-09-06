483ec1de78da14d3156a70d6bba9b1f65c5f2c28
Eden Reich <eden.reich@gmail.com>
2020-09-06 23:33:25 +0200

    improve installation

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
+set -e
 
 install_for_linux()
 {
-	apt-get update	
-
-	if [ ! `which npm` ] || [ ! `which node` ]; then
-		echo '>> npm or node is not installed. installing nodejs...'
-		apt-get install -y nodejs
-	fi
+	echo '>> Updating...'
+	apt-get update && apt-get -y upgrade	
 
 	if [ ! `which git` ]; then
 		echo '>> git is not installed. Installing git...'
@@ -19,8 +16,23 @@ install_for_linux()
 		apt-get install -y curl
 	fi
 
+	if [ ! `which npm` ] || [ ! `which node` ]; then
+		echo '>> nodejs is not installed. installing nodejs...'
+		if [ ! `which lsb_release` ]; then
+			apt-get install -y lsb-release
+		fi
+		echo "deb https://deb.nodesource.com/node_12.x `lsb_release -cs` main" > /etc/apt/sources.list.d/nodesource.list
+		echo "deb-src https://deb.nodesource.com/node_12.x `lsb_release -cs` main" >> /etc/apt/sources.list.d/nodesource.list
+		apt-get install -y gnupg
+		curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+
+		apt-get update && apt-get install -y nodejs
+	fi
+
 	if [ ! `which vim` ]; then
 		echo '>> vim is not installed. Installing vim...'
+		apt-get install -y software-properties-common
+		add-apt-repository -y ppa:jonathonf/vim
 		apt-get install -y vim
 	fi
 
@@ -32,13 +44,13 @@ install_for_linux()
 	echo '>> Downloading vim configurations...'
 	curl -sSL https://raw.githubusercontent.com/edenreich/vim-on-speeds/master/.vimrc -o $HOME/.vimrc
 
-	vim +'PlugInstall --sync'
+	vim +PlugInstall
 }
 
 install_for_macos()
 {
 	if [ ! vim ]; then
-		echo 'vim is not installed. Installing vim..'
+		echo '>> vim is not installed. Installing vim..'
 		brew install vim
 	fi
 }
